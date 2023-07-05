c983a72078ebe1994960b413b350f3c47113ec2f
niqzart <qwert45hi@yandex.ru>
2023-07-06 00:42:28 +0300

    feat: functional tests for subscribing

--- backend/tests/functional/conftest.py
+++ backend/tests/functional/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
+from app.main import sio
 from app.twex.twex_db import Twex
+from tests.testing import AsyncSIOTestClient
 
 
 @pytest.fixture()
@@ -8,3 +10,12 @@ async def source_twex(file_name: str) -> Twex:
     twex = Twex(file_name=file_name)
     await twex.save()
     return twex
+
+
+@pytest.fixture()
+async def roomed_sender(
+    sender: AsyncSIOTestClient,
+    source_twex: Twex,
+) -> AsyncSIOTestClient:
+    sio.enter_room(sender.sid, f"{source_twex.file_id}-publishers")
+    return sender
