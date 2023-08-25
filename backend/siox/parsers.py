13f7115430a22898cfc95772a5b39927fb5bc7ac
niqzart <qwert45hi@yandex.ru>
2023-08-25 23:51:54 +0300

    feat: split socketio-x into files

---
+++ backend/siox/parsers.py
@@ -0,0 +1,201 @@
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable, Iterator
+from inspect import Parameter, Signature, signature
+from typing import Annotated, Any, TypeVar, get_args, get_origin
+
+from pydantic import BaseModel, create_model
+from pydantic._internal._typing_extra import eval_type_lenient
+from socketio import AsyncNamespace  # type: ignore
+
+from app.common.sockets import Ack
+from siox.markers import Depends, SessionID
+from siox.results import (
+    CEContext,
+    ClientEvent,
+    Dependency,
+    ExpandableArgument,
+    Runnable,
+)
+from siox.types import AnyCallable, LocalNS
+
+T = TypeVar("T")
+
+
+class SPContext(BaseModel, arbitrary_types_allowed=True):
+    arg_types: list[type | ExpandableArgument] = []
+    first_expandable_argument: ExpandableArgument | None = None
+    signatures: dict[AnyCallable, SignatureParser] = {}
+
+
+class SignatureParser:
+    def __init__(
+        self,
+        func: Callable[..., T | Awaitable[T]],
+        context: SPContext,
+        event_context: CEContext,
+        local_ns: LocalNS | None = None,
+        destination: Runnable | None = None,
+    ) -> None:
+        self.func = func
+        self.signature: Signature = signature(func)
+        self.local_ns: LocalNS = local_ns or {}
+        self.destination: Runnable = destination or Runnable(func)
+        self.context: SPContext = context
+        self.event_context: CEContext = event_context
+        self.unresolved: set[AnyCallable] = set()
+
+    def parse_positional_only(self, param: Parameter, ann: Any) -> None:
+        raise NotImplementedError
+
+    def parse_typed_kwarg(self, param: Parameter, type_: type) -> None:
+        if issubclass(type_, RequestSignature):
+            self.event_context.request_positions.append((self.destination, param.name))
+        elif self.context.first_expandable_argument is None:
+            # TODO better error message or auto-creation of first expandable
+            raise Exception("No expandable arguments found")
+        else:
+            self.context.first_expandable_argument.add_field(
+                param.name, type_, param.default, self.destination
+            )
+
+    def parse_double_annotated_kwarg(
+        self, param: Parameter, type_: Any, decoded: Any
+    ) -> None:
+        if isinstance(decoded, Depends):
+            dependency = self.context.signatures.get(decoded.dependency)
+            if dependency is None:
+                dependency = DependencySignature(
+                    decoded.dependency,
+                    self.context,
+                    self.event_context,
+                    self.local_ns,
+                )
+                dependency.parse()
+                self.context.signatures[decoded.dependency] = dependency
+            elif not isinstance(dependency, DependencySignature):
+                raise Exception(
+                    f"Can't add destination to {type(dependency)}"
+                )  # TODO errors
+            dependency.the_dep.destinations.setdefault(self.destination, []).append(
+                param.name
+            )
+            self.unresolved.add(decoded.dependency)
+        elif isinstance(decoded, SessionID):
+            self.event_context.sid_positions.append((self.destination, param.name))
+        elif isinstance(decoded, int):
+            if len(self.context.arg_types) <= decoded:
+                raise Exception(
+                    f"Param {param} can't be saved to [{decoded}]: "
+                    f"only {len(self.context.arg_types)} are present"
+                )
+            argument_type = self.context.arg_types[decoded]
+            if isinstance(argument_type, ExpandableArgument):
+                argument_type.add_field(
+                    param.name, type_, param.default, self.destination
+                )
+            else:
+                raise Exception(
+                    f"Param {param} can't be saved to "
+                    f"{argument_type} at [{decoded}]"
+                )
+        else:
+            raise NotImplementedError(f"Parameter {type_} {decoded} not supported")
+
+    def parse_annotated_kwarg(self, param: Parameter, *args: Any) -> None:
+        if len(args) != 2:
+            raise Exception("Annotated supported with 2 args only")
+        self.parse_double_annotated_kwarg(param, *args)
+
+    def parse(self) -> None:
+        for param in self.signature.parameters.values():
+            annotation: Any = param.annotation  # TODO type the annotation
+            if isinstance(annotation, str):
+                global_ns = getattr(self.func, "__globals__", {})
+                annotation = eval_type_lenient(annotation, global_ns, self.local_ns)
+
+            if param.kind == param.POSITIONAL_ONLY:
+                self.parse_positional_only(param, annotation)
+            elif isinstance(annotation, type):
+                self.parse_typed_kwarg(param, annotation)
+            elif get_origin(annotation) is Annotated:
+                self.parse_annotated_kwarg(param, *get_args(annotation))
+            else:
+                raise NotImplementedError  # TODO errors
+
+
+class DependencySignature(SignatureParser):
+    def __init__(
+        self,
+        func: AnyCallable,
+        context: SPContext,
+        event_context: CEContext,
+        local_ns: dict[str, Any],
+    ) -> None:
+        self.the_dep: Dependency = Dependency(func)  # TODO naming
+        super().__init__(
+            func, context, event_context, local_ns, destination=self.the_dep
+        )
+
+    def parse_positional_only(self, param: Parameter, ann: Any) -> None:
+        raise Exception("No positional args allowed for dependencies")  # TODO errors
+
+
+class RequestSignature(SignatureParser):
+    def __init__(self, handler: Callable[..., Ack], ns: type | None = None):
+        super().__init__(
+            func=handler,
+            context=SPContext(signatures={handler: self}),
+            event_context=CEContext(),
+            local_ns=ns and ns.__dict__,  # type: ignore[arg-type]  # assume bool(type) is True
+        )
+
+    def parse_positional_only(self, param: Parameter, ann: Any) -> None:
+        if issubclass(ann, BaseModel):
+            expandable_argument = ExpandableArgument(ann)
+            if self.context.first_expandable_argument is None:
+                self.context.first_expandable_argument = expandable_argument
+            self.context.arg_types.append(expandable_argument)
+        else:
+            self.context.arg_types.append(ann)
+
+    def generate_positional_fields(self) -> Iterator[type]:
+        for arg_type in self.context.arg_types:
+            if isinstance(arg_type, ExpandableArgument):
+                yield arg_type.convert()
+            else:  # TODO remove the isinstance check
+                yield arg_type
+
+    def resolve_dependencies(self) -> Iterator[Dependency]:
+        layer: list[SignatureParser]
+        while len(self.unresolved) != 0:  # TODO errors for cycles in DR
+            layer = [
+                dependency
+                for dependency in self.context.signatures.values()
+                if len(dependency.unresolved) == 0
+            ]
+            yield from (
+                dependency.the_dep
+                for dependency in layer
+                if isinstance(dependency, DependencySignature)
+            )
+            for signature in self.context.signatures.values():
+                for resolved in layer:
+                    signature.unresolved.discard(resolved.func)
+
+    def extract(self) -> ClientEvent:
+        self.parse()
+        return ClientEvent(
+            event_context=self.event_context,
+            arg_model=create_model(  # type: ignore[call-overload]
+                "InputModel",  # TODO model name from event & namespace(?)
+                **{
+                    str(i): (ann, ...)
+                    for i, ann in enumerate(self.generate_positional_fields())
+                },
+            ),
+            arg_types=self.context.arg_types,
+            arg_count=len(self.context.arg_types),
+            dependency_order=list(self.resolve_dependencies()),
+            destination=self.destination,
+        )
---
+++ backend/siox/results.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable, Iterator
+from contextlib import AsyncExitStack, asynccontextmanager, contextmanager
+from inspect import (
+    Parameter,
+    isasyncgenfunction,
+    iscoroutinefunction,
+    isgeneratorfunction,
+)
+from typing import Any, TypeVar
+
+from pydantic import BaseModel, ValidationError, create_model
+from socketio import AsyncNamespace  # type: ignore
+
+from app.common.sockets import AbortException, Ack
+
+T = TypeVar("T")
+
+
+class ExpandableArgument:  # TODO split into this & argument parser
+    def __init__(self, base: type[BaseModel]) -> None:
+        self.base = base
+        self.fields: dict[str, tuple[type, Any]] = {}
+        self.destinations: dict[str, list[Runnable]] = {}
+
+    def add_field(
+        self, name: str, type_: Any, default: Any, destination: Runnable
+    ) -> None:
+        if default is Parameter.empty:
+            default = ...
+        passed_field = type_, default
+        existing_field = self.fields.get(name)
+        if existing_field is None:
+            self.fields[name] = passed_field
+        elif existing_field != passed_field:
+            raise NotImplementedError("Duplicate with a different type")  # TODO errors
+        self.destinations.setdefault(name, []).append(destination)
+
+    def convert(self) -> type[BaseModel]:
+        return create_model(  # type: ignore[call-overload, no-any-return]
+            f"{self.base.__qualname__}.Expanded",
+            __base__=self.base,
+            **self.fields,
+        )
+
+    def clean(self, result: BaseModel) -> BaseModel:
+        return self.base.model_validate(
+            result.model_dump(include=set(self.base.model_fields.keys()))
+        )
+
+
+class Runnable:
+    def __init__(self, func: Callable[..., T | Awaitable[T]]) -> None:
+        self.func = func
+        self.args: tuple[Any, ...] = ()
+        self.kwargs: dict[str, Any] = {}
+
+    async def run(self) -> T:
+        if iscoroutinefunction(self.func):
+            return await self.func(*self.args, **self.kwargs)  # type: ignore[no-any-return]
+        elif callable(self.func):
+            return self.func(*self.args, **self.kwargs)  # type: ignore[return-value]
+        raise Exception("Handler is not callable")
+
+
+class Dependency(Runnable):
+    def __init__(self, func: Callable[..., T | Awaitable[T]]) -> None:
+        super().__init__(func)
+        self.destinations: dict[Runnable, list[str]] = {}
+
+    async def resolve(self, stack: AsyncExitStack) -> Any:  # TODO move out
+        if isasyncgenfunction(self.func):
+            return await stack.enter_async_context(
+                asynccontextmanager(self.func)(*self.args, **self.kwargs)
+            )
+        elif isgeneratorfunction(self.func):
+            return stack.enter_context(
+                contextmanager(self.func)(*self.args, **self.kwargs)
+            )
+        return await self.run()
+
+
+class CEContext(BaseModel, arbitrary_types_allowed=True):  # TODO naming
+    request_positions: list[tuple[Runnable, str]] = []
+    sid_positions: list[tuple[Runnable, str]] = []
+
+
+class ClientEvent:
+    def __init__(
+        self,
+        event_context: CEContext,
+        arg_model: type[BaseModel],
+        arg_types: list[type | ExpandableArgument],
+        arg_count: int,
+        dependency_order: list[Dependency],
+        destination: Runnable,
+    ):
+        self.event_context = event_context
+        self.arg_model = arg_model
+        self.arg_types = arg_types
+        self.arg_count = arg_count
+        self.dependency_order = dependency_order
+        self.destination = destination
+
+    def parse_arguments(self, arguments: tuple[Any, ...]) -> Iterator[Any]:
+        # TODO argument parser
+        converted = self.arg_model.model_validate(
+            {str(i): ann for i, ann in enumerate(arguments)}
+        )
+        for i, arg_type in enumerate(self.arg_types):
+            result: Any = getattr(converted, str(i))
+            if isinstance(arg_type, ExpandableArgument):
+                yield arg_type.clean(result)
+                for field_name, destinations in arg_type.destinations.items():
+                    value = getattr(result, field_name)
+                    for destination in destinations:
+                        destination.kwargs[field_name] = value
+            else:
+                yield result
+
+    async def execute(self, event_name: str, sid: str, *arguments: Any) -> Ack | None:
+        # TODO use *actual* Request data-object
+        if len(arguments) != self.arg_count:
+            return Ack(
+                code=422,
+                data=f"Required {self.arg_count}, but {len(arguments)} given",
+            )
+
+        try:
+            self.destination.args = tuple(self.parse_arguments(arguments))
+        except (ValidationError, AttributeError) as e:
+            return Ack(code=422, data=str(e))
+
+        for value, destinations in (  # TODO more expandability
+            (self, self.event_context.request_positions),
+            (sid, self.event_context.sid_positions),
+        ):
+            for destination, field_name in destinations:
+                destination.kwargs[field_name] = value
+
+        try:
+            async with AsyncExitStack() as stack:
+                for dependency in self.dependency_order:
+                    value = await dependency.resolve(stack)
+                    for destination, field_names in dependency.destinations.items():
+                        for field_name in field_names:
+                            destination.kwargs[field_name] = value
+
+                # call the function
+                return await self.destination.run()
+            # this code is, in fact, reachable
+            # noinspection PyUnreachableCode
+            return None  # TODO `with` above can lead to no return
+        except AbortException as e:
+            return e.ack
